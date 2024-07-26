04589144ddb8d1fa022efdda1af9b652d527501b
kajahl <kacperjahl37@gmail.com>
2024-07-26 02:50:21 +0200

    UsersService & Its tests

--- src/modules/users/services/users/users.service.spec.ts
+++ src/modules/users/services/users/users.service.spec.ts
@@ -1,10 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { getDataSourceToken, getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from 'src/models/entities/User.entity';
+import { DataSource, Repository } from 'typeorm';
+import { CreateUser } from 'src/models/types/User';
+import { BadRequestException, ConflictException, NotFoundException } from '@nestjs/common';
 
 describe('UsersService', () => {
     let service: UsersService;
+    let dataSource: DataSource;
+    let userRepository: Repository<UserEntity>;
+
+    const firstUser: CreateUser = {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'UPPERCASE@test.com',
+        password: 'password',
+        profileName: 'john_doe',
+        roles: [],
+        permissions: [],
+    }
+    const secondUser: CreateUser = {
+        firstName: 'Ann',
+        lastName: 'Doe',
+        email: 'lowercase@TEST.com',
+        password: 'password',
+        profileName: 'ann_doe',
+        roles: [],
+        permissions: [],
+    }
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -23,9 +47,150 @@ describe('UsersService', () => {
         }).compile();
 
         service = module.get<UsersService>(UsersService);
+        dataSource = module.get<DataSource>(getDataSourceToken());
+        userRepository = module.get<Repository<UserEntity>>(getRepositoryToken(UserEntity));
+    });
+
+    beforeEach(async () => {
+        await dataSource.synchronize(true); // Reset database before each test
+    });
+
+    afterEach(async () => {
+        await dataSource.synchronize(true); // Clean database after each test
+    });
+
+    afterAll(async () => {
+        await dataSource.destroy(); // Close the connection after all tests are done
     });
 
     it('should be defined', () => {
         expect(service).toBeDefined();
     });
+
+    describe('create()', () => {
+        it('should create a user', async () => {
+            const user = await service.create(firstUser);
+            expect(user).toBeDefined();
+            expect(user.id).toBeDefined();
+            expect(user.email).toBe(firstUser.email.toLowerCase());
+        });
+
+        it('should throw an error if the email is already taken', async () => {
+            await service.create(firstUser);
+            await expect(service.create(firstUser)).rejects.toThrow(ConflictException);
+        });
+    });
+
+    describe('findAll()', () => {
+        beforeEach(async () => {
+            await service.create(firstUser);
+            await service.create(secondUser);
+        });
+
+        it('should return an array of users', async () => {
+            const users = await service.findAll();
+            expect(users).toBeInstanceOf(Array);
+            expect(users.length).toBe(2);
+            expect(users.find(user => user.email === firstUser.email.toLowerCase())).toBeDefined();
+            expect(users.find(user => user.email === secondUser.email.toLowerCase())).toBeDefined();
+        });
+    });
+    
+    describe('findById()', () => {
+        let userId: number;
+
+        beforeEach(async () => {
+            const user = await service.create(firstUser);
+            userId = user.id;
+        });
+
+        it('should return a user by id', async () => {
+            const user = await service.findById(userId);
+            expect(user).toBeDefined();
+            expect(user.email).toBe(firstUser.email.toLowerCase());
+        });
+
+        it('should return undefined if user is not found', async () => {
+            await expect(service.findById(999)).rejects.toThrow(NotFoundException);
+        });
+    });
+
+    describe('update()', () => {
+        let userId: number;
+
+        beforeEach(async () => {
+            const user = await service.create(firstUser);
+            userId = user.id;
+        });
+
+        it('should update a user', async () => {
+            const updatedUser = await service.update(userId, { firstName: 'Jane' });
+            expect(updatedUser).toBeDefined();
+            expect(updatedUser.firstName).toBe('Jane');
+        });
+
+        it('should throw an error if the email is already taken', async () => {
+            await service.create(secondUser);
+            await expect(service.update(userId, { email: secondUser.email })).rejects.toThrow(ConflictException);
+        });
+
+        it('should throw an error if the user is not found', async () => {
+            await expect(service.update(999, { firstName: 'Jane' })).rejects.toThrow(NotFoundException);
+        });
+
+        it('should throw an error if updating password', async () => {
+            await expect(service.update(userId, { password: 'password' })).rejects.toThrow(BadRequestException);
+        });
+    });
+
+    describe('updatePassword()', () => {
+        let userId: number;
+
+        beforeEach(async () => {
+            const user = await service.create(firstUser);
+            userId = user.id;
+        });
+
+        it('should update a user password', async () => {
+            const newPassword = 'new_password';
+            const updatedUser = await service.updatePassword(userId, {
+                password: newPassword,
+                confirmPassword: newPassword,
+            });
+            expect(updatedUser).toBe(true);
+            const user = await userRepository.findOne({ where: { id: userId } });
+            expect(user.password).not.toEqual(newPassword);
+            expect(service.comparePassword(newPassword, user.password)).toBe(true);
+        });
+
+        it('should throw an error if the passwords do not match', async () => {
+            await expect(service.updatePassword(userId, { password: 'password', confirmPassword: 'wrong' })).rejects.toThrow(BadRequestException);
+        });
+
+        it('should throw an error if the user is not found', async () => {
+            await expect(service.updatePassword(999, { password: 'password', confirmPassword: 'password' })).rejects.toThrow(NotFoundException);
+        });
+    });
+
+    describe('remove()', () => {
+        let userId: number;
+
+        beforeEach(async () => {
+            const user = await service.create(firstUser);
+            userId = user.id;
+        });
+
+        it('should remove a user', async () => {
+            const result = await service.remove(userId);
+            expect(result).toBe(true);
+            const user = await userRepository.findOne({ where: { id: userId } });
+            expect(user).toBeNull();
+        });
+
+        it('should throw an error if the user is not found', async () => {
+            await expect(service.remove(999)).rejects.toThrow(NotFoundException);
+        });
+    });
+
+    
 });
--- src/modules/users/services/users/users.service.ts
+++ src/modules/users/services/users/users.service.ts
@@ -1,35 +1,73 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from 'src/models/entities/User.entity';
 import { CreateUser, UpdateUser } from 'src/models/types/User';
-import { Repository } from 'typeorm';
+import { DeleteResult, QueryFailedError, Repository, UpdateResult } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import ChangePasswordDto from 'src/models/dtos/users/ChangePassword.dto';
 
 @Injectable()
 export class UsersService {
-    constructor(
-        @InjectRepository(UserEntity) private usersRepository: Repository<UserEntity>
-    ) {}
+    constructor(@InjectRepository(UserEntity) private usersRepository: Repository<UserEntity>) {}
+
+    readonly verbose = false; // TODO: Where to store this?
 
     // Temp - hash will be in authService
     hashPassword(password: string) {
         return bcrypt.hashSync(password, 10);
     }
+    comparePassword(password: string, hash: string) {
+        return bcrypt.compareSync(password, hash);
+    }
     // End temp
 
+    /**
+     * Create a new user.
+     * @param createUser - The user data to create.
+     * @returns The created user.
+     */
     async create(createUser: CreateUser) {
         createUser.password = this.hashPassword(createUser.password);
         createUser.email = createUser.email.toLowerCase();
-        return this.usersRepository.save(createUser);
-    }
-    
-    async findAll() {
-        return this.usersRepository.find(); // TODO Serialize
+        const user = this.usersRepository.create(createUser);
+        return this.usersRepository.save(user).catch((err: any) => {
+            if (this.verbose) console.warn(err);
+            if (err.code === '23505') throw new ConflictException('User with this email already exists');
+            throw new InternalServerErrorException('#TODO_CODE_001');
+        });
+        return user;
     }
 
+    /**
+     * Returns all users.
+     * @returns Array of all users.
+     */
+    async findAll() {
+        return this.usersRepository.find().catch((err) => {
+            if (this.verbose) console.warn(err);
+            throw new InternalServerErrorException('#TODO_CODE_006');
+        }); // TODO Serialize
+    }
+
+    /**
+     * Returns a user by ID.
+     * @param id - The ID of the user to find.
+     * @returns The user with the given ID.
+     * @throws BadRequestException if the user is not found.
+     * @throws InternalServerErrorException for other errors.
+     */
     async findById(id: number) {
-        return this.usersRepository.findOne({ where: { id } }); // TODO Serialize
+        const user = await this.usersRepository
+            .findOne({ where: { id } })
+            .then((u) => {
+                return u;
+            })
+            .catch((err) => {
+                if (this.verbose) console.warn(err);
+                throw new InternalServerErrorException('#TODO_CODE_002');
+            }); // TODO Serialize
+        if (user === null) throw new NotFoundException('User not found');
+        return user;
     }
 
     // TODO
@@ -37,16 +75,62 @@ export class UsersService {
         throw new Error('Method not implemented.');
     }
 
+    /**
+     * Updates a user by ID.
+     * @param id - The ID of the user to update.
+     * @param updateUser - The user data to update.
+     * @returns The updated user.
+     * @throws BadRequestException if:
+     * - (1) The user is not found.
+     * - (2) The email already exists.
+     * - (3) The password is included in the update data.
+     * @throws InternalServerErrorException for other errors.
+     */
     async update(id: number, updateUser: UpdateUser) {
-        return this.usersRepository.update(id, updateUser);
+        if (updateUser.email) updateUser.email = updateUser.email.toLowerCase();
+        if (updateUser.password)
+            throw new BadRequestException('Use the change password endpoint to update the password');
+        const result = await this.usersRepository.update(id, updateUser).catch((err: any) => {
+            if (this.verbose) console.warn(err);
+            if (err.code === '23505') throw new ConflictException('User with this email already exists');
+            throw new InternalServerErrorException('#TODO_CODE_003');
+        });
+        if (result.affected === 0) throw new NotFoundException('User not found');
+        return this.findById(id);
     }
 
-    async updatePassword(id: number, updateUser: ChangePasswordDto) {
-        if(updateUser.password !== updateUser.confirmPassword) throw new BadRequestException('Passwords do not match');
-        return this.usersRepository.update(id, { password: this.hashPassword(updateUser.password) });
+    /**
+     * Updates a user's password by ID.
+     * @param id - The ID of the user to update.
+     * @param updatePassword - The new password data.
+     * @returns The result of the update operation.
+     * @throws BadRequestException if the passwords do not match.
+     */
+    async updatePassword(id: number, updatePassword: ChangePasswordDto) {
+        if (updatePassword.password !== updatePassword.confirmPassword)
+            throw new BadRequestException('Passwords do not match');
+        const result = await this.usersRepository
+            .update(id, { password: this.hashPassword(updatePassword.password) })
+            .catch((err: any) => {
+                if (this.verbose) console.warn(err);
+                throw new InternalServerErrorException('#TODO_CODE_004');
+            });
+        if (result.affected === 0) throw new NotFoundException('User not found');
+        return true;
     }
 
+    /**
+     * Deletes a user by ID.
+     * @param id - The ID of the user to delete.
+     * @returns True if the user was deleted.
+     * @throws BadRequestException if the user is not found.
+     */
     async remove(id: number) {
-        return this.usersRepository.delete(id);
+        const result = await this.usersRepository.delete(id).catch((err: any) => {
+            if (this.verbose) console.warn(err);
+            throw new InternalServerErrorException('#TODO_CODE_005');
+        });
+        if (result.affected === 0) throw new NotFoundException('User not found');
+        return true;
     }
 }
