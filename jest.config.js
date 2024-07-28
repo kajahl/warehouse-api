e8b5cfb4e2b615ba37e9f92fd23d1ad132771671
kajahl <kacperjahl37@gmail.com>
2024-07-29 01:47:47 +0200

    Separated unit and integration jest configs

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,50 @@
-module.exports = {
+const unitTestsConfig = {
+    displayName: 'unit',
+    testRegex: ".*\\.spec\\.unit\\.ts$",
     moduleFileExtensions: [
         "js",
         "json",
         "ts"
     ],
     rootDir: "src",
-    testRegex: ".*\\.spec\\.ts$",
     transform: {
         "^.+\\.(t|j)s$": "ts-jest"
     },
     collectCoverageFrom: [
         "**/*.(t|j)s"
     ],
-    coverageDirectory: "../coverage",
+    coverageDirectory: "../coverage/unit",
     testEnvironment: "node",
     moduleNameMapper: {
         '^src/(.*)$': '<rootDir>/$1'
     },
     detectOpenHandles: true,
+}
+
+const integrationTestsConfig = {
+    displayName: 'integration',
+    testRegex: ".*\\.spec\\.int\\.ts$",
+    moduleFileExtensions: [
+        "js",
+        "json",
+        "ts"
+    ],
+    rootDir: "src",
+    transform: {
+        "^.+\\.(t|j)s$": "ts-jest"
+    },
+    collectCoverageFrom: [
+        "**/*.(t|j)s"
+    ],
+    coverageDirectory: "../coverage/integration",
+    testEnvironment: "node",
+    moduleNameMapper: {
+        '^src/(.*)$': '<rootDir>/$1'
+    },
+    detectOpenHandles: true,
+}
+
+
+module.exports = {
+    projects: [unitTestsConfig, integrationTestsConfig]
 };
