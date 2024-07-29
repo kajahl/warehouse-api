86827e78098891a023f03d20907fa14bdce40229
kajahl <kacperjahl37@gmail.com>
2024-07-29 16:27:33 +0200

    Moved user registration endpoint to auth controller

--- src/modules/auth/controllers/auth/auth.controller.ts
+++ src/modules/auth/controllers/auth/auth.controller.ts
@@ -1,14 +1,31 @@
-import { Controller, Get, HttpCode, HttpStatus, Inject, Post, Request, Response, Session, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Inject, Post, Request, Response, Session, UseGuards } from '@nestjs/common';
 import { AuthService } from '../../services/auth/auth.service';
 import { LocalAuthGuard } from 'src/utils/guards/LocalAuth.guard';
 import { IsAuthenticatedGuard } from 'src/utils/guards/IsAuthenticated.guard';
 import { JwtAuthGuard } from 'src/utils/guards/JwtAuth.guard';
+import RegisterUserDto from 'src/models/dtos/users/RegisterUser.dto';
+import { UsersService } from 'src/modules/users/services/users/users.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(@Inject() private authService: AuthService) {}
+    constructor(
+        @Inject() private authService: AuthService,
+        @Inject() private usersService: UsersService
+    ) {}
 
     // Local
+
+    @Post('signup')
+    async register(
+        @Body() body: RegisterUserDto
+    ) {
+        return this.usersService.create({
+            ...body,
+            roles: [],
+            permissions: []
+        });
+    }
+
     @Post('login')
     @UseGuards(LocalAuthGuard)
     @HttpCode(HttpStatus.OK)
